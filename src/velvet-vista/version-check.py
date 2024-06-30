7f68f18eebb977e885d15e7b34e1035d23d5d902
Alix Fullerton <fullertonalix0@gmail.com>
2024-06-30 14:59:42 -0600

    Working on dev environment

---
+++ src/velvet-vista/version-check.py
@@ -0,0 +1,27 @@
+"""
+    Velvet Vista
+    Version Check
+"""
+
+
+class VersionCheck:
+
+    def run(self):
+        print("hello")
+
+    def get_version_github(self):
+        """
+        """
+        print("getting version")
+
+    def get_sonarr(self):
+        url = "https://github.com/Sonarr/Sonarr/releases"
+        
+
+
+if __name__ == "__main__":
+    print("hello")
+    VersionCheck().run()
+
+
+# End File: politeauthority/velvet-vista/src/velvet-vista/version-check.py
