38ff9c3604ecc5f122b2825ceeb56560eea6bc90
Alix Fullerton <fullertonalix0@gmail.com>
2024-06-30 14:08:16 -0600

    Getting a dev dockerfile to work with

---
+++ src/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+# from polite_lib.version import version
+
+setup(
+    name="velvet-vista",
+    description="Velvet Vista",
+    version=version,
+    author="Alix",
+    author_email="alix@politeauthority.io",
+    packages=[
+        # "polite_lib",
+        # "polite_lib.utils",
+        # "polite_lib.file_tools",
+        # "polite_lib.fmt",
+        # "polite_lib.notify",
+    ],
+)
+
+# End File: politeauthority/velvet-vista/src/setup.py
